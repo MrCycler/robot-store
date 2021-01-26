d5dad0fc2c4e69b056037d8604410e91bb6fc10b
Juan Mendoza <mendoza.jm@pucp.edu.pe>
2021-01-25 23:16:12 -0500

    add: *ngFor arrays

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'robot-store';
+  robots = ['Wall-e','C3PO','R2D2','B-max'];
+  addItem(){
+    this.robots.push("Robot sin nombre");
+  };
+  deleteItem(index:number){
+    this.robots.splice(index,1);
+  };
 }
